e88a75df50e92c4243cb26d5c1fc510c21db9045
joel081112 <joel081112@icloud.com>
2021-04-02 17:19:01 +0100

    Graphs and averages

--- ards_site/settings/base.py
+++ ards_site/settings/base.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mathfilters',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- home/urls.py
+++ home/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 
-
 urlpatterns = [
     path('', views.member_view, name='members'),
     path('scorecard/firstXI/', views.match_view_firsts, name='matches'),
     path('scorecard/secondXI/', views.match_view_seconds, name='matches'),
+    path('averages/firstXI/', views.averages_view_firsts, name='averages'),
+    path('averages/secondXI/', views.averages_view_seconds, name='averages'),
     path('fixtures/firstXI/', views.fixture_view_firsts, name='fixtures'),
     path('fixtures/secondXI/', views.fixture_view_seconds, name='fixtures'),
     path('players/firstXI/', views.players_view_firsts, name='players'),
@@ -19,5 +20,7 @@ urlpatterns = [
     path('member/add', views.add_new_member, name='createadd'),
     path('view_match/<match_id>', views.view_selected_match, name='viewmatch'),
     path('view_fixture/<match_id>', views.view_selected_fixture, name='viewfixture'),
+    path('api/data/', views.get_data, name='api-data'),
+    path('api/chart/data/', views.ChartData.as_view(), name='api-chart-data')
 
 ]
